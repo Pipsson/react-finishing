f4841e312544500d635bee7774f48f736ee45b36
Pipsson <juliusmallya8420@gmail.com>
2025-03-25 20:20:42 +0300

    This commit include ideas on landing data like list, normal variables and so on.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+    const title = "Title"
+    const body = "this is the body of the  blog"
+    const comments = [
+        {
+            id: 1, text: "Comment 1",
+        },
+        {
+            id: 2, text: "Comment 2",
+        },
+        {
+            id: 3, text: "Comment 3",
+        },
+        {
+            id: 4, text: "Comment 4",
+        },
+
+
+    ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <div className="container">
+            <h1 className="title">{title}</h1>
+            <p className="body">{body}</p>
+        </div>
+        <div className="comments">
+            <p>Comments ({
+                comments.length
+            })</p>
+            <ul>
+                {  comments.map((comment, index) => (
+                     <li key={index}>
+                         {comment.text}
+                     </li>
+                )             )        }
+            </ul>
+        </div>
     </div>
   );
 }
